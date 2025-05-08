3589656ca95925d94dee471bfbd93e7eccb2e8e2
Khaled Ghareeb <khaledghareeb147@gmail.com>
2025-05-08 23:38:27 +0300

    Initialization

---
+++ sketch_may7a/sketch_may7a.ino
@@ -0,0 +1,13 @@
+#include<Keypad.h>
+#include<Servo.h>
+#include<Wire.h>
+#include<LiquidCrystal_I2C.h>
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
